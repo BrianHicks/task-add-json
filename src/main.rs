9cdc67df8c43d57c4ee87e90ddee781763729aa5
Brian Hicks <brian@brianthicks.com>
2025-02-20 11:57:03 -0600

    drop anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 pub mod task;
 
-use anyhow::{Context, Result};
 use std::{env::args, io::stdout};
 use task::Task;
 
 fn main() {
-    if let Err(err) = run() {
-        eprintln!("{:?}", err);
-        std::process::exit(1);
-    }
-}
-
-fn run() -> Result<()> {
     let task = Task::from_iter(args().skip(1));
 
-    serde_json::to_writer(stdout(), &task).context("could not write JSON to stdout")?;
-
-    Ok(())
+    if let Err(err) = serde_json::to_writer(stdout(), &task) {
+        eprintln!("Error serializing task: {}", err);
+        std::process::exit(1)
+    }
 }
