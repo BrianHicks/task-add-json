79d20ee944ffc6fde2ad33a23f20af825b42f852
Brian Hicks <brian@brianthicks.com>
2025-02-25 11:15:55 -0600

    test inputs

--- tests/task_parity.rs
+++ tests/task_parity.rs
@@ -6,7 +6,6 @@ use std::process::{Command, Stdio};
 
 fn date_strategy() -> impl Strategy<Value = String> {
     prop_oneof![
-        "now",
         "today",
         "sod",
         "eod",
@@ -17,7 +16,7 @@ fn date_strategy() -> impl Strategy<Value = String> {
         "1st",
         "28th",
         "midsommar",
-        r#"\d{4}-(01|02|03|04|05|06|07|08|09|10|11|12)-(01|05|10|15|20|25)"#
+        r#"2[0-9]{3}-(01|02|03|04|05|06|07|08|09|10|11|12)-(01|05|10|15|20|25)"#
     ]
 }
 
@@ -25,8 +24,20 @@ fn due_strategy() -> impl Strategy<Value = String> {
     ("due", date_strategy()).prop_map(|(key, value)| format!("{key}:{value}"))
 }
 
-fn words_strategy() -> impl Strategy<Value = Vec<String>> {
-    vec(prop_oneof!["a", "b", "c"], 1..3)
+fn word_strategy() -> impl Strategy<Value = String> {
+    prop_oneof!["a", "b", "c"]
+}
+
+fn arg_strategy() -> impl Strategy<Value = String> {
+    prop_oneof![word_strategy(), due_strategy()]
+}
+
+fn args_strategy() -> impl Strategy<Value = Vec<String>> {
+    (word_strategy(), vec(arg_strategy(), 1..10)).prop_map(|(arg, mut rest)| {
+        rest.insert(0, arg);
+
+        rest
+    })
 }
 
 fn export_only(dir: &Path) -> serde_json::Map<String, serde_json::Value> {
@@ -35,69 +46,88 @@ fn export_only(dir: &Path) -> serde_json::Map<String, serde_json::Value> {
             .arg("export")
             .env("TASKDATA", dir)
             .output()
-            .expect("task to export successfully")
+            .expect("successful call to `task export`")
             .stdout,
     )
     .expect("could not decode value");
 
-    value.as_array().unwrap()[0].as_object().unwrap().clone()
+    value
+        .as_array()
+        .expect("array as the output of `task export`")
+        .first()
+        .expect("`task export` to have at least one item")
+        .as_object()
+        .expect("the first item in `task export` to be an object")
+        .clone()
 }
 
 const BIN: &str = env!("CARGO_BIN_EXE_task-json");
 
-#[test]
-fn task_parity() {
-    let args = vec!["walk", "the", "dog", "due:today"];
+proptest! {
+    #[test]
+    fn task_parity(args in args_strategy()) {
+        // parse command line to json and import with our binary
+        let from_task_json = {
+            let temp = tempdir::TempDir::new("task-json").expect("tempdir to be created");
 
-    // parse command line to json and import with our binary
-    let from_task_json = {
-        let temp = tempdir::TempDir::new("task-json").unwrap();
+            let task_json = Command::new(BIN)
+                .args(&args)
+                .stdout(Stdio::piped())
+                .spawn()
+                .expect("successful call to `task-json`");
 
-        let task_json = Command::new(BIN)
-            .args(&args)
-            .stdout(Stdio::piped())
-            .spawn()
-            .unwrap();
+            Command::new("task")
+                .arg("import")
+                .env("TASKDATA", temp.path())
+                .stdin(task_json.stdout.unwrap())
+                .output()
+                .expect("successful call to `task import`");
 
-        Command::new("task")
-            .arg("import")
-            .env("TASKDATA", temp.path())
-            .stdin(task_json.stdout.unwrap())
-            .output()
-            .unwrap();
+            export_only(temp.path())
+        };
 
-        export_only(temp.path())
-    };
+        // use task to add the same command line
+        let from_task_add = {
+            let temp = tempdir::TempDir::new("task-json").unwrap();
 
-    // use task to add the same command line
-    let from_task_add = {
-        let temp = tempdir::TempDir::new("task-json").unwrap();
+            let status = Command::new("task")
+                .arg("add")
+                .args(&args)
+                .env("TASKDATA", temp.path())
+                .status()
+                .expect("successful call to `task add`");
 
-        Command::new("task")
-            .arg("add")
-            .args(&args)
-            .env("TASKDATA", temp.path())
-            .output()
-            .unwrap();
+            assert!(status.success(), "`task add` failed");
 
-        export_only(temp.path())
-    };
+            export_only(temp.path())
+        };
 
-    let mut all_keys = HashSet::new();
-    all_keys.extend(from_task_json.keys());
-    all_keys.extend(from_task_add.keys());
+        let mut all_keys = HashSet::new();
+        all_keys.extend(from_task_json.keys());
+        all_keys.extend(from_task_add.keys());
 
-    // We don't care about the UUID
-    all_keys.remove(&String::from("uuid"));
+        // We don't care about the UUID
+        all_keys.remove(&String::from("uuid"));
 
-    // Testing entry date is flakey, unless we set it explicitly (TODO: check this)
-    all_keys.remove(&String::from("entry"));
+        // Testing entry date is flakey, unless we set it explicitly
+        // (TODO: check for explicit sets)
+        all_keys.remove(&String::from("entry"));
 
-    for key in all_keys {
-        assert_eq!(
-            from_task_json.get(key),
-            from_task_add.get(key),
-            "Args: `{args:?}`\n\nFrom task-json: {from_task_json:#?}\n\nFrom `task add`: {from_task_add:#?}\n\n`{key}` was not the same:\n"
-        );
+        // Testing modified date is flakey, unless we set it explicitly
+        // (TODO: check for explicit sets)
+        all_keys.remove(&String::from("modified"));
+
+        // Urgency is a float (so not always directly comparable) and it's
+        // derived from the other attributes we care about.
+        all_keys.remove(&String::from("urgency"));
+
+        for key in all_keys {
+            prop_assert_eq!(
+                from_task_json.get(key),
+                from_task_add.get(key),
+                "`{}` was not the same.\n\nFrom task-json: {:#?}\n\nFrom `task add`: {:#?}\n",
+                key, from_task_json, from_task_add
+            );
+        }
     }
 }
